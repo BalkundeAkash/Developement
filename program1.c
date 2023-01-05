146ab7997f82008274a8064922baef925e267881
BalkundeAkash <104627797+BalkundeAkash@users.noreply.github.com>
2023-01-05 15:57:36 +0530

    Add files via upload

---
+++ program1.c
@@ -0,0 +1,40 @@
+//Write a program to perform additon of Two Numbers
+
+///////////////////////////////////////////////////////////////////////////////////
+//
+//Function Name : Addition
+//Description   : Used to perform addtion of two numbers
+//Input         : Integer , Integer
+//Output        : Integer
+//Date          : 5/1/2023
+//Author        : Akash Dilip Balkunde
+//
+///////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>>
+
+int main()
+{
+    int i=0;
+    int j=0;
+    int k=0;
+
+    printf("Enter First Number\n");
+    scanf("%d",&i);
+
+    printf("Enter Second Number\n");
+    scanf("%d",&j);
+
+    k = i+j;
+
+    printf("Addition is: %d\n",k);
+
+    return 0;
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+//
+// Input: 11 10
+// Output: 21
+//
+///////////////////////////////////////////////////////////////////////////////////
